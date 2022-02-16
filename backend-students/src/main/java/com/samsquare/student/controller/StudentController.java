bb1c62e0a9eaa94814520d814b367ee3c340edc9
sammed-sankonatti <sammed.sankonatti45@gmail.com>
2022-02-16 16:33:54 +0530

    code to add multiple data

--- backend-students/src/main/java/com/samsquare/student/controller/StudentController.java
+++ backend-students/src/main/java/com/samsquare/student/controller/StudentController.java
@@ -1,7 +1,9 @@
 package com.samsquare.student.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import com.samsquare.student.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,8 +28,8 @@ public class StudentController {
 	
 	//create student API :
 	@PostMapping("/students")
-	public Student createStudent(@RequestBody Student student) {
-		return studentRepository.save(student);
+	public void createStudent(@RequestBody List<Student> student) {
+		 studentRepository.saveAll(student);
 	}
 
 	@GetMapping("/students/{id}")
@@ -36,8 +38,16 @@ public class StudentController {
 	}
 
 	@PutMapping("/students")
-	public Student updateStudentDetails(@RequestBody Student student){
-		return studentRepository.save(student);
+	public void updateStudentDetails(@RequestBody List<Student> students){
+		List<Student> updatedStudents = new ArrayList<>();
+		for(Student student : students){
+			Student  temp = studentRepository.findById(student.getId()).orElseThrow(() -> new ResourceNotFoundException(" student not found with id = "+student.getId()));
+			temp.setFirstName(student.getFirstName());
+			temp.setLastName(student.getLastName());
+			temp.setEmailId(student.getEmailId());
+			updatedStudents.add(temp);
+		}
+		 studentRepository.saveAll(updatedStudents);
 	}
 
 	@DeleteMapping("/students/{id}")
